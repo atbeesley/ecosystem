3ad6e7e04e8f30f23520a0aec7c18eef2c471883
user <user@users-MacBook-Pro.local>
2019-09-17 17:05:20 +0100

    adds river_has_name and fish_amount functions

--- river.rb
+++ river.rb
@@ -2,9 +2,18 @@ class River
 
   attr_reader :name, :fish_amount
 
-def initialize(name, cash)
-  @name = name
-  @fish_amount = fish_amount
-end
+  def initialize(name, cash)
+    @name = name
+    @fish = []
+  end
+
+  def river_has_name
+    return @river.name()
+  end
+
+  def fish_amount()
+    return @fish.count()
+  end
+
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -5,12 +5,17 @@ require_relative('../river')
 class RiverTest < MiniTest::Test
 
   def setup()
-    @river= River.new("Nile", 5000)
+    @river = River.new("Nile", 5000)
   end
-
 
   def test_river_has_name()
     assert_equal("Nile", @river.name)
   end
+
+  def test_fish_amount()
+    assert_equal(0, @river.fish_amount())
+  end
+
+
 
 end
