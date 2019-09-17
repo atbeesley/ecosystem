0cae361294d179277aa42e3f11b2e6d08672da21
user <user@users-MacBook-Pro.local>
2019-09-17 17:56:33 +0100

    bear_type

--- bear.rb
+++ bear.rb
@@ -1,12 +1,14 @@
 class Bear
 
-  attr_reader :name
+  attr_reader :name, :type
 
-  def initialize(name)
+  def initialize(name, type)
     @name = name
+    @type = type
   end
 
-
-
+  def bear_type()
+    return @bear.type()
+  end
 
 end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -5,11 +5,15 @@ require_relative('../bear.rb')
 class BearTest < MiniTest::Test
 
   def setup()
-    @bear = Bear.new("Yogi")
+    @bear = Bear.new("Yogi", "Grizzly")
   end
 
   def test_bear_has_name()
     assert_equal("Yogi", @bear.name())
+  end
+
+  def test_bear_type()
+    assert_equal("Grizzly", @bear.type())
   end
 
 
